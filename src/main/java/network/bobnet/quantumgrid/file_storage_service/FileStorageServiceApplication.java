c198c91c652bb8cb80fd9da9afccf80997f468e4
smaso_JaszayN <bob@bobnet.network>
2024-09-02 17:34:41 +0200

    QUAN-48 Set up project structure using Spring Boot

---
+++ src/main/java/network/bobnet/quantumgrid/file_storage_service/FileStorageServiceApplication.java
@@ -0,0 +1,16 @@
+package network.bobnet.quantumgrid.file_storage_service;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Generated
+@SpringBootApplication
+public class FileStorageServiceApplication {
+
+	@Generated
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageServiceApplication.class, args);
+	}
+
+}
